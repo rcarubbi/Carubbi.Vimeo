03c9a70ca18ab5bd7f53c71a29d85c81444c0c50
Raphael Carubbi <rcarubbi@gmail.com>
2018-08-22 16:20:16 +0100

    Initial Commit

---
+++ Carubbi.Vimeo/Client.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace Carubbi.Vimeo
+{
+    public class Client
+    {
+        private const string BASE_API = "https://api.vimeo.com/";
+        private readonly string _accessToken;
+
+        public Client(string accessToken)
+        {
+            _accessToken = accessToken;
+        }
+
+        public string GetDownloadLink(long videoId)
+        {
+            using (var wc = new WebClient())
+            {
+                var json = wc.DownloadString($"{BASE_API}videos/{videoId}?access_token={_accessToken}");
+                dynamic obj = JsonConvert.DeserializeObject(json);
+                return obj.download[0].link;
+            }
+        }
+    }
+}
